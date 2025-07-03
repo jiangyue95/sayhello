b927ce622ac3945af72c5b5d6c91e972ab8fb5dc
jiangyue95 <jiangyue0013@gmail.com>
2025-07-03 19:16:51 +0100

    add APIs

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"sayhello/internal/message"
+	"time"
 )
 
 func RegisterRoutes() *gin.Engine {
 	r := gin.Default()
+
+	// Open CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Accept", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	api := r.Group("/api")
+	{
+		api.GET("/messages", message.ListMessagesHandler)
+		api.POST("/messages", message.CreateMessageHandler)
+	}
 
 	msg := r.Group("/messages")
 	{
